f7d60e85ba111ee5656fa072ae1413a03e1a676f
Tormi Tabor <tormi.tabor@gmail.com>
2024-03-14 19:24:16 +0200

    Rename crs constant to epsg3301.

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ const kataster = L.tileLayer.wms('https://kaart.maaamet.ee/wms/alus-geo?', {
 })
 
 // Create a custom CRS for EPSG:3301
-var crs = new L.Proj.CRS(
+const epsg3301 = new L.Proj.CRS(
   'EPSG:3301',
   '+proj=lcc +lat_1=59.33333333333334 +lat_2=58 +lat_0=57.51755393055556 +lon_0=24 +x_0=500000 +y_0=6375000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
   {
@@ -54,7 +54,7 @@ const yp_mk = L.tileLayer.wms('https://planeeringud.ee/plank/wms?', {
   format: 'image/png',
   transparent: true,
   layers: 'yp_maakasutus',
-  crs: crs, // Set the custom CRS for EPSG:3301
+  crs: epsg3301, // Set the custom CRS for EPSG:3301
   attribution:
     'Kiht: <a href="https://planeeringud.ee/plank-web/#/planning/detail/20100625" target="_blank">Saue valla ÜP maakasutus</a>',
 })
@@ -64,7 +64,7 @@ const yp_tr = L.tileLayer.wms('https://planeeringud.ee/plank/wms?', {
   format: 'image/png',
   transparent: true,
   layers: 'yp_transport',
-  crs: crs, // Set the custom CRS for EPSG:3301
+  crs: epsg3301, // Set the custom CRS for EPSG:3301
   attribution:
     'Kiht: <a href="https://planeeringud.ee/plank-web/#/planning/detail/20100625" target="_blank">Saue valla ÜP transport</a>',
 })
